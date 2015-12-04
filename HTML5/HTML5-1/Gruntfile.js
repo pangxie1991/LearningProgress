7833367d18301c22aab7caabeef5b9b482a23d7d
LucasIcarus <lucasicarus@hotmail.com>
2015-12-05 00:55:48 +0800

    Home 15.12.05 Stu.

---
+++ HTML5/HTML5-1/Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        modernizr : {
+            dist: {
+                "dest" : "javascript/modernizr.js",
+                "parseFiles": true,
+                "customTests": [],
+                "devFile": "/PATH/TO/modernizr-dev.js",
+                "outputFile": "/PATH/TO/modernizr-output.js",
+                "tests": [
+                    // Tests
+                ],
+                "extensibility": [
+                    "setClasses"
+                ],
+                "uglify": true
+            }
+        },
+        cssmin : {
+            combine: {
+                files: {
+                    'css/release/compress.css': ['css/*.css'] // 指定合并的CSS文件 ['css/base.css', 'css/global.css']
+                }
+            },
+            minify: {
+                options: {
+                    keepSpecialComments: 0, /* 删除所有注释 */
+                    banner: '/* minified css file */'
+                },
+                files: {
+                    'css/release/master.min.css': ['css/master.css']
+                }
+            }
+        }
+    });
+
+    // Load the plugins.
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks("grunt-modernizr");
+
+
+    // Default task(s).
+    grunt.registerTask('default', ['modernizr']);
+
+};
--- HTML5/HTML5-1/javascript/fallback_form.js
+++ HTML5/HTML5-1/javascript/fallback_form.js
@@ -1,37 +1,28 @@
-/*** Created by Fancy on 2015/12/3 0003.*/
-function hasColorSupport(){
-    element = document.createElement("input");
-    element.setAttribute("type", "color");
-    var hasColorType = (element.type === "color");
-
-    if(hasColorType){
-        var testString = "foo";
-        element.value = testString;
-        hasColorType = (element.value != testString);
-    }
-    return(hasColorType);
-}
-
-
-var applyColorPicker = function(){
+/**
+ * Created by Fancy on 2015/12/4 0004.
+ */
+var applyColorPicker = function (){
     $('input[type=color]').simpleColor();
 };
 
-if (!hasColorSupport()){
+function fallback (path,onload) {
     var script = document.createElement('script');
-    script.src = "javascript/jquery.simple-color.js";
-
-    if(script.readyState){   //使用IE浏览器
+    script.src = path;
+    document.getElementsByTagName("head")[0].appendChild(script);
+    if (script.readyState) {   //使用IE浏览器
         script.onreadystatechange = function () {
-            if (this.readyState === 'loaded' || this.readyState === 'complete'){
+            if (this.readyState === 'loaded' || this.readyState === 'complete') {
                 script.onreadystatechange = null;
-                applyColorPicker();
+                onload();
             }
         };
-    }else {
+    } else {
         //使用其他浏览器
-        script.onload = applyColorPicker;
+        script.onload = onload;
     }
+}
 
-    document.getElementsByTagName("head")[0].appendChild(script);
+if (!Modernizr.inputtypes.color) {
+    fallback("javascript/jquery.simple-color.js",applyColorPicker);
+    applyColorPicker();
 }
