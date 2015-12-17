bfebcff9f000a0c18cab57ed3632d046ebec18e7
LucasIcarus <lucasicarus@hotmail.com>
2015-12-17 23:41:43 +0800

    Home 15.12.17 Stu.js has no end

--- lab_project/lab_schedule_bootstrap/js/main.js
+++ lab_project/lab_schedule_bootstrap/js/main.js
@@ -1,8 +1,10 @@
-/**
- * Created by Fancy on 2015/12/16 0016.
- */
-var showSomething = function (node1,node2){
-    node1.appendChild(node2)
-}
+///**
+// * Created by Fancy on 2015/12/16 0016.
+// */
+//
+//if ($("label[data-target = '#selection_of_time']").hasClass("active")) {
+//    $("label[data-target = '#selection_of_time']").addClass("data-toggle");
+//}
+//
 
 
