9954c7844a2436f8dbe26ab15a74fa1b23407702
LucasIcarus <lucasicarus@hotmail.com>
2015-12-19 22:38:35 +0800

    Home 15.12.19 After a date

--- lab_project/lab_schedule_bootstrap/js/main.js
+++ lab_project/lab_schedule_bootstrap/js/main.js
@@ -1,26 +1,4 @@
-///**
-// * Created by Fancy on 2015/12/16 0016.
-// */
-//
-//if ($("label[data-target = '#selection_of_time']").hasClass("active")) {
-//    $("label[data-target = '#selection_of_time']").addClass("data-toggle");
-//}
-//
-
-
-//var time_selector = $("#selection_of_time");
-//var time_trigger = $("label[data-target = '#selection_of_time']");
-//time_selector.on("shown.bs.collapse",function() {
-//    time_trigger.attr("data-target","");
-//    time_trigger.prevAll().click(function () {
-//        time_selector.collapse('hide');
-//    })
-//})
-//
-//time_selector.on("hidden.bs.collapse",function() {
-//    time_trigger.attr("data-target","#selection_of_time");
-//})
-
+/* Created by Fancy on 2015/12/16 0016.*/
 function doSelectorRight(trigger) {
     var the_selector = $("#"+trigger+"");
     var the_trigger = $("label[data-target = '#" + trigger + "']");
@@ -30,12 +8,22 @@ function doSelectorRight(trigger) {
             the_selector.collapse('hide');
         })
     })
-
     the_selector.on("hidden.bs.collapse", function () {
         the_trigger.attr("data-target", "#"+trigger+"");
     })
 }
 
 doSelectorRight("selection_of_time");
+
 doSelectorRight("selection_of_date");
+
+var reservation = $("#reservation");
+reservation.on("shown.bs.collapse",function () {
+    $(".jumbotron").hide();
+})
+
+$(".btn-danger").click(function () {
+    reservation.collapse('hide');
+    $(".jumbotron").show();
+})
 
