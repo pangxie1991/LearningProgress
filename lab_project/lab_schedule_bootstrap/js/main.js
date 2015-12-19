f99ee9c4d3cf5447ba11d2a43d1516f04b3432a4
LucasIcarus <lucasicarus@hotmail.com>
2015-12-19 11:44:51 +0800

    Home 15.12.19 before a date

--- lab_project/lab_schedule_bootstrap/js/main.js
+++ lab_project/lab_schedule_bootstrap/js/main.js
@@ -8,3 +8,34 @@
 //
 
 
+//var time_selector = $("#selection_of_time");
+//var time_trigger = $("label[data-target = '#selection_of_time']");
+//time_selector.on("shown.bs.collapse",function() {
+//    time_trigger.attr("data-target","");
+//    time_trigger.prevAll().click(function () {
+//        time_selector.collapse('hide');
+//    })
+//})
+//
+//time_selector.on("hidden.bs.collapse",function() {
+//    time_trigger.attr("data-target","#selection_of_time");
+//})
+
+function doSelectorRight(trigger) {
+    var the_selector = $("#"+trigger+"");
+    var the_trigger = $("label[data-target = '#" + trigger + "']");
+    the_selector.on("shown.bs.collapse", function () {
+        the_trigger.attr("data-target", "");
+        the_trigger.prevAll().click(function () {
+            the_selector.collapse('hide');
+        })
+    })
+
+    the_selector.on("hidden.bs.collapse", function () {
+        the_trigger.attr("data-target", "#"+trigger+"");
+    })
+}
+
+doSelectorRight("selection_of_time");
+doSelectorRight("selection_of_date");
+
