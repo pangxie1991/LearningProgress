768d973e88577b9e93913512bfeed070868286c1
LucasIcarus <lucasicarus@hotmail.com>
2015-12-20 21:24:53 +0800

    Home 15.12.20 stop lab_project temporarily

--- lab_project/lab_schedule_bootstrap/js/main.js
+++ lab_project/lab_schedule_bootstrap/js/main.js
@@ -1,6 +1,6 @@
 /* Created by Fancy on 2015/12/16 0016.*/
-function doSelectorRight(trigger) {
-    var the_selector = $("#"+trigger+"");
+function makeSelectorRight(trigger) {
+    var the_selector = $("#" + trigger + "");
     var the_trigger = $("label[data-target = '#" + trigger + "']");
     the_selector.on("shown.bs.collapse", function () {
         the_trigger.attr("data-target", "");
@@ -9,21 +9,89 @@ function doSelectorRight(trigger) {
         })
     })
     the_selector.on("hidden.bs.collapse", function () {
-        the_trigger.attr("data-target", "#"+trigger+"");
+        the_trigger.attr("data-target", "#" + trigger + "");
     })
 }
 
-doSelectorRight("selection_of_time");
+function getDateString(AddDayCounts) {
+    var origin = new Date();
+    origin.setDate(origin.getDate() + AddDayCounts);
+    var y = origin.getFullYear();
+    var m = origin.getMonth() + 1;
+    var d = origin.getDate();
+    return  y + "." + m + "." + d;
+}
 
-doSelectorRight("selection_of_date");
+makeSelectorRight("selection_of_time");
+
+makeSelectorRight("selection_of_date");
 
 var reservation = $("#reservation");
-reservation.on("shown.bs.collapse",function () {
+reservation.on("shown.bs.collapse", function () {
     $(".jumbotron").hide();
 })
 
-$(".btn-danger").click(function () {
+$("#reservation .btn-danger").click(function () {
     reservation.collapse('hide');
     $(".jumbotron").show();
 })
+
+$("#reservation .btn[data-target='#confirm_window']").click(function () {
+    var name_active = $("#name_panel label.active span"),
+        name_length = name_active.length,
+        name_string = "",
+        project_active = $("#project_panel label.active span"),
+        project_length = project_active.length,
+        project_string = "",
+        i,
+        j = 1;
+    for (i = 0; i < name_length; i++) {
+        name_string += name_active[i].innerHTML;
+        if (i < name_length - 1) {
+            name_string += "、";
+        }
+    }
+    if ($("#project_panel label:eq(0)").hasClass("active")){
+        project_string += "磁控溅射："+$("#input_of_sputtering input").val()+" & ";
+        j+=1;
+    }
+    if ($("#project_panel label:eq(8)").hasClass("active")){
+        project_string += $("#input_of_experiment input").val()+" & ";
+        j+=1;
+    }
+    if ($("#project_panel label:eq(15)").hasClass("active")){
+        project_string += $("#input_of_others input").val()+" & ";
+        j+=1;
+    }
+    for (i = 0; i<project_length;i++){
+        if(project_active[i].innerHTML === "磁控溅射" ||project_active[i].innerHTML === "其他外出实验" ||project_active[i].innerHTML === "其他事宜"){
+            continue;
+        }
+        project_string += project_active[i].innerHTML;
+        if (i<project_length-j){
+            project_string += " & ";
+        }
+    }
+    $(".modal-body tr:eq(0)").children()[1].innerHTML = project_string;
+    $(".modal-body tr:eq(3)").children()[1].innerHTML = name_string;
+    if ($("#time_panel label.btn:eq(4)").hasClass("active")) {
+        $(".modal-body tr:eq(1)").children()[1].innerHTML = getDateString(0);
+    }if ($("#time_panel label.btn:eq(5)").hasClass("active")){
+        $(".modal-body tr:eq(1)").children()[1].innerHTML = getDateString(1);
+    }if ($("#time_panel label.btn:eq(6)").hasClass("active")){
+        $(".modal-body tr:eq(1)").children()[1].innerHTML = $("#dtp_input2").val()+" 到 "+$("#dtp_input3").val();
+    }
+    if ($("#time_panel label.btn:eq(0)").hasClass("active")){
+        $(".modal-body tr:eq(2)").children()[1].innerHTML = "9:00~11:00";
+    }if ($("#time_panel label.btn:eq(1)").hasClass("active")){
+        $(".modal-body tr:eq(2)").children()[1].innerHTML = "14:30~17:00";
+    }if ($("#time_panel label.btn:eq(2)").hasClass("active")){
+        $(".modal-body tr:eq(2)").children()[1].innerHTML = "全天";
+    }if ($("#time_panel label.btn:eq(3)").hasClass("active")){
+        $(".modal-body tr:eq(2)").children()[1].innerHTML = $("#dtp_input4").val()+"~"+$("#dtp_input5").val();
+    }
+})
+
+$(".modal-body tr:eq(2) td:eq(1)").innerHTML = "AAAAAAA";
+
 
